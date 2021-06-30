f10908f449fb2ab8e53766deda8ca41b7fb980ed
Sarangapani Varadharajan <sarangapani.singapore@gmail.com>
2021-06-30 11:00:59 +0800

    Added Examples in download

---
+++ SourceDump/src/com/upgrad/sarang/FundamentalsOfProgramming/VariablesDataTypes/bool.java
@@ -0,0 +1,24 @@
+package com.upgrad.sarang.FundamentalsOfProgramming.VariablesDataTypes;
+
+import java.util.*;
+public class bool {
+    public static void main(String[] args) {
+
+        //relational ops
+        int a,b;
+        Scanner input=new Scanner(System.in);
+        a = input.nextInt();
+        b = input.nextInt();
+
+        // a = 4; b = 5
+        System.out.println(a>=b);
+        System.out.println(a==b);
+
+        //logical ops
+        boolean x=true, y=false, z=true;
+
+        // precedence order
+        boolean result= x || y && !z;
+        System.out.println(result);
+    }
+}
---
+++ SourceDump/src/com/upgrad/sarang/FundamentalsOfProgramming/VariablesDataTypes/casting2.java
@@ -0,0 +1,18 @@
+package com.upgrad.sarang.FundamentalsOfProgramming.VariablesDataTypes;
+
+public class casting2 {
+    public static void main(String[] args)
+    {
+        int marks1 = 20;
+        int marks2 = 40;
+        int marks3 = 50;
+
+        double average = ((double)marks1 + (double)marks2 + (double)marks3)/3;
+        System.out.println(average);
+
+        // another example
+        char x = 'a';
+        int value = (int)x;
+        System.out.println(value);
+    }
+}
